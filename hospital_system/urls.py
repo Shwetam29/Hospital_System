ca3e866c3e9a085e17b4cf3445b466bad1496864
Shwetam29 <shwetamunde22@gmail.com>
2024-07-05 02:58:51 -0700

    done dashboard

--- hospital_system/urls.py
+++ hospital_system/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path('doctor_dashboard',doctor_dashboard,name="doctor_dashboard"),
     path('login',login,name="login"),
     path('signin',signin,name="signin"),
-    # path('add',add_todo,name="add_todo"),
     path('signout',signout,name="logout"),
-    # path('delete-todo/<int:id>',delete_todo,name="delete_todo"),
-    # path('change-status/<int:id>/<str:status>',change_todo,name="change_todo"),
+   
 ]
